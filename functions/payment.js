6609420b2fcb3b60c7ef2bb19c1924b2ae463890
Armando Barrera <armando.barrera@ppro.com>
2021-01-07 00:41:38 +0100

    Add log to debug

--- functions/payment.js
+++ functions/payment.js
@@ -1,26 +1,47 @@
 exports.handler = async (event) => {
-    const {headers, body} = event;
+    const {headers, body, httpMethod} = event;
     console.log("headers", headers, "body", body);
     if (!headers || !headers.apiKey || headers.apiKey !== "secureKey") {
+        if (!headers.apiKey) {
+            console.log("no apiKey");
+        }
+        if (headers.apiKey !== "secureKey") {
+            console.log(`api key not match, ${headers.apiKey} received`)
+        }
         return {
             statusCode: 400,
             body:JSON.stringify({message: "Bad request"})
         }
     }
     
-    let content;
-
-    try {
-        content = parseContent(body);
-    } catch (error) {
+    if (httpMethod !== "GET" || httpMethod !== "POST") {
         return {
-            statusCode: 500,
-            body:JSON.stringify({message: `${error}`})
+            statusCode: 405,
+            body: JSON.stringify({message: "Method not allowed"})
         }
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({data:{content:`${content}`, headers:`${headers}`}})
+    if (httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify()
+        }
+    }
+
+    if (httpMethod === "POST") {
+        let content;
+        try {
+            content = parseContent(body);
+            return {
+                statusCode: 200,
+                body: JSON.stringify()
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body:JSON.stringify({message: `${error}`})
+            }
+        }
+        
     }
 }
