617d61f073cf73504c900a2243855d054dd13e4b
Armando Barrera <armando.barrera@ppro.com>
2021-01-07 00:10:02 +0100

    Payment testing

---
+++ functions/payment.js
@@ -0,0 +1,27 @@
+const {validateRequest, handleError, parseContent} = require("../lib/request");
+
+exports.handler = async (event) => {
+    const {headers, body} = event;
+    if (!headers || !headers.authorization) {
+        return {
+            statusCode: 400,
+            body:JSON.stringify({message: "Bad request"})
+        }
+    }
+    
+    let content;
+
+    try {
+        content = parseContent(body);
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body:JSON.stringify({message: `${error}`})
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({data:{content:`${content}`, headers:`${headers}`}})
+    }
+}
