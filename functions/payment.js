5d89e7431504341f28563b4190b46b33dbd0ba7a
Armando Barrera <armando.barrera@ppro.com>
2021-01-07 13:15:54 +0100

    Update api POST method

--- functions/payment.js
+++ functions/payment.js
@@ -3,8 +3,8 @@ exports.handler = async (event) => {
     console.log("headers", headers, "body", body);
     if (!headers || !headers.api_key || headers.api_key !== "secureKey") {
         return {
-            statusCode: 400,
-            body:JSON.stringify({message: "Bad request"})
+            statusCode: 401,
+            body:JSON.stringify({error: "Incorrect authentication"})
         }
     }
     
@@ -17,15 +17,107 @@ exports.handler = async (event) => {
         case "POST":
             let content;
             try {
-                content = parseContent(body);
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify()
+                content = JSON.parse(body);
+
+                const {amount, currency, country} = content;
+
+                if (amount < 0 || amount > 1000) {
+                    return {
+                        statusCode: 400,
+                        body: JSON.stringify({error: "Amount not allowed"})
+                    }
                 }
+
+                if (country === "DE") {
+                    const orderId = Math.floor(Math.random()*100000);
+                    if (currency === "EUR") {
+                        return {
+                            statusCode: 200, 
+                            body: JSON.stringify(
+                                {
+                                    data: [
+                                        {
+                                            orderId,
+                                            sessionId: `test-${orderId}`,
+                                            amount: amount,
+                                            status: true,
+                                            message: "Transaction succeeded"
+                                        },
+                                    ]},
+                                    {
+                                        responseCode: "0",
+                                    }
+                            )
+                        }
+                    } else {
+                        return {
+                            statusCode: 400,
+                            body: JSON.stringify({error: `Currency not allowed on selected country`})
+                        }
+                    }
+                } else if (country === "AU") {
+                    if (currency === "AUD") {
+                        return {
+                            statusCode: 200, 
+                            body: JSON.stringify(
+                                {
+                                    data: [
+                                        {
+                                            orderId,
+                                            sessionId: `test-${orderId}`,
+                                            amount: amount,
+                                            status: true,
+                                            message: "Transaction succeeded"
+                                        },
+                                    ]},
+                                    {
+                                        responseCode: "0",
+                                    }
+                            )
+                        }
+                    } else {
+                        return {
+                            statusCode: 400,
+                            body: JSON.stringify({error: `Currency not allowed on selected country`})
+                        }
+                    }
+                } else if (country === "SG") {
+                    if (currency === "SGD") {
+                        return {
+                            statusCode: 200, 
+                            body: JSON.stringify(
+                                {
+                                    data: [
+                                        {
+                                            orderId,
+                                            sessionId: `test-${orderId}`,
+                                            amount: amount,
+                                            status: true,
+                                            message: "Transaction succeeded"
+                                        },
+                                    ]},
+                                    {
+                                        responseCode: "0",
+                                    }
+                            )
+                        }
+                    } else {
+                        return {
+                            statusCode: 400,
+                            body: JSON.stringify({error: `Currency not allowed on selected country`})
+                        }
+                    }
+                } else {
+                    return {
+                        statusCode: 400,
+                        body: JSON.stringify({error: "Country not allowed"})
+                    }
+                }
+
             } catch (error) {
                     return {
                         statusCode: 500,
-                        body:JSON.stringify({message: `${error}`})
+                        body:JSON.stringify({error: `${error}`})
                     }
                 }
         default:
